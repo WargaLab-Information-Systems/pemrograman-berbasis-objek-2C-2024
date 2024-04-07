bb0d7853766591b10fb683197b182b23f520586d
ALIF <alfathnaa8@gmail.com>
2024-04-08 00:11:09 +0700

    230441100059-AlfathNashrullahArif-MuhammadAkmalNabilHibrizi-MODUL3

---
+++ modul-3/230441100059-AlfathNashrullahArif/Kendaraan/src/kendaraan/Mobil.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kendaraan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Mobil extends Kendaraan{
+
+        Mobil(String merek, String model, char status, int tahunKendaraan){
+        super("Mobil", merek, model, status, tahunKendaraan);
+   
+    }
+    
+}
