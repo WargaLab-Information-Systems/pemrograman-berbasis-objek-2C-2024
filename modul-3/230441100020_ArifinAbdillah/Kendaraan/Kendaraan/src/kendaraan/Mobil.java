f8056249d210f2cdcee04c7a703b53c467fa297b
arifinabdillah <arifinabdillah259@gmail.com>
2024-04-09 13:24:50 +0700

    230441100020-ARIFIN ABDILLAH-MUHAMMAD AKMAL NABIL HIBRIZI-MODUL 3

---
+++ modul-3/230441100020_ArifinAbdillah/Kendaraan/Kendaraan/src/kendaraan/Mobil.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kendaraan;
+
+public class Mobil extends Kendaraan{
+        
+        Mobil(String merek, String model, char status, int tahun){
+        super("Mobil", merek, model, status, tahun);
+
+    }
+}
---
+++ modul-3/230441100020_ArifinAbdillah/Kendaraan/Kendaraan/src/kendaraan/Motor.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kendaraan;
+
+public class Motor extends Kendaraan {
+   
+    Motor(String merek, String model, char status, int tahun){
+        super("Motor", merek, model, status, tahun);
+    }
+}
+
+
