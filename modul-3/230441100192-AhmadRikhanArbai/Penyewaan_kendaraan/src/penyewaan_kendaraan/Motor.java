5662bf09b7127f24025895b3c9ff06c4696f8c11
r1kh4n <ahmadrikanarbai354@gmail.com>
2024-04-09 11:12:23 +0700

    REHAN

---
+++ modul-3/230441100192-AhmadRikhanArbai/Penyewaan_kendaraan/src/penyewaan_kendaraan/Motor.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package penyewaan_kendaraan;
+
+/**
+ *
+ * @author HP
+ */
+public class Motor extends Kendaraan{
+    public Motor(String merek, String model, char status, int tahun) {
+        super("Motor",merek, model, status, tahun);
+    } 
+}
