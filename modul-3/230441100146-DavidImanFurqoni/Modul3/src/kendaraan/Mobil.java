e56942f8e1a11bf2185e4f893f86914ea84964e9
DavidImanF <david61if61@gmail.com>
2024-04-16 21:25:26 +0700

    Faroid_M3_146

---
+++ modul-3/230441100146-DavidImanFurqoni/Modul3/src/kendaraan/Mobil.java
@@ -0,0 +1,14 @@
+package kendaraan;
+
+/**
+ *
+ * @author DAVID IMAN FURQONI
+ */
+public class Mobil extends kendaraan {
+//    private int seater;
+    
+    public Mobil (String jenis, String merk, String model, String status, int tahunKendaraan ){
+        super (jenis, merk, model, status, tahunKendaraan);
+
+    }
+}
