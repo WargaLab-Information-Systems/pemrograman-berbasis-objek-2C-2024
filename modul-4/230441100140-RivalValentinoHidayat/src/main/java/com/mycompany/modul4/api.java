2932d8b70adfd10135c0dcc37fd288491ad3c744
RivalV124 <rivaljaya123@gmail.com>
2024-04-22 20:51:54 +0700

    Faroid_PBO2C_M4_140

---
+++ modul-4/230441100140-RivalValentinoHidayat/src/main/java/com/mycompany/modul4/api.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+// panjang.java
+package com.mycompany.modul4;
+
+public class api extends Converter {
+    @Override
+    public float converter(float meter) {
+        return meter / 1000.0f;
+    }
+}
---
+++ modul-4/230441100140-RivalValentinoHidayat/src/main/java/com/mycompany/modul4/tanah.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+// waktu.java
+package com.mycompany.modul4;
+
+public class tanah extends Converter {
+    @Override
+    public float converter(float kilogram) {
+        return kilogram * 2.20462f;
+    }
+}
+
+
