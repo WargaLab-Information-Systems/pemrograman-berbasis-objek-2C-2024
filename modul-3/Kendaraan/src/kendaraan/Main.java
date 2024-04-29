a569d50225360682854678c8b4cd8ca87a2852dd
RoidFawwaz <roidfawwaz@gmail.com>
2024-04-29 10:50:15 +0700

    Faroid_PBO2C_M3_183

---
+++ modul-3/Kendaraan/src/kendaraan/Main.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kendaraan;
+import java.util.Scanner;
+
+public class Main {   
+public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      int jumlahKendaraan;
+         System.out.print("Masukkan jumlah kendaraan yang akan di input: ");
+         jumlahKendaraan = input.nextInt();
+         input.nextLine();
+         Kendaraan[] daftarKendaraan = new Kendaraan[jumlahKendaraan];
+         
+         // Memasukkan data kendaraan
+         for (int i = 0; i < jumlahKendaraan; i++) {
+             System.out.println("\nMasukkan data kendaraan ke-" + (i + 1));
+             System.out.print("Jenis Kendaraan (Mobil/Motor): ");
+             String jenis = input.nextLine();
+             System.out.print("Merek Kendaraan: ");
+             String merek = input.nextLine();
+             System.out.print("Model Kendaraan: ");
+             String model = input.nextLine();
+             System.out.print("Status Kendaraan(T/D:) ");
+             char status = input.nextLine().charAt(0);
+             System.out.print("Tahun Kendaraan: ");
+             int tahun = input.nextInt();
+             input.nextLine();
+        
+             //Kondisi Pilihan
+             if (jenis.equalsIgnoreCase("Mobil")) {
+                 daftarKendaraan[i] = new Mobil(merek, model,
+                         status, tahun);
+             } else if (jenis.equalsIgnoreCase("Motor")) {
+                 daftarKendaraan[i] = new Motor(merek, model,
+                         status, tahun);
+             } else {
+                 System.out.println("Jenis kendaraan tidak valid.");
+             }
+         }
+
+         
+         // Menampilkan informasi kendaraan yang telah dimasukkan
+        System.out.println("\nDaftar Kendaraan:");
+        for (Kendaraan kendaraan : daftarKendaraan) {
+            kendaraan.displayInfo();
+             if (kendaraan.getstatuskendaraan() == 'T'){
+                System.out.println("Status Kendaraan: Tersedia\n");
+            } else if (kendaraan.getstatuskendaraan() == 'D'){
+                System.out.println("Status Kendaraan:  Disewakan\n");
+        }
+      }
+   }
+}
+
---
+++ modul-3/Kendaraan/src/kendaraan/NewClass.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kendaraan;
+
+/**
+ *
+ * @author HP
+ */
+class NewClass {
+    String hewan;
+    char tumbuhan;
+    int angka;
+    
+    public static void main(String[] args) {
+   
+        NewClass ncls = new NewClass();
+        ncls.hewan= "ayam";
+        ncls.tumbuhan='m';
+        ncls.angka= 3;
+        
+        System.out.println(ncls.hewan);
+        System.out.println(ncls.tumbuhan);
+        System.out.println(ncls.angka+" kali");
+    }  
+} 
