4627df0422104d02e6dffe79de61fa6385bf7701
arifinabdillah <arifinabdillah259@gmail.com>
2024-05-22 23:01:31 +0700

    230441100020-ARIFIN ABDILLAH-MUHAMMAD AKMAL NABIL HIBRIZI-MODUL 6

---
+++ modul-6/modul 6/modul6/src/modul6/wifi.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul6;
+
+
+interface wifi {
+    void cariJaringan();
+    void hubungkanJaringan();
+    void lupakanJaringan();
+}
