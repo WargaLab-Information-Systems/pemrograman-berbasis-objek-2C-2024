ea25d2ac2f738a8875a8786519f82d8846027050
miftahuzzainabrori <abrorelfrd13@gmail.com>
2024-05-26 23:19:51 +0700

    MODUL5_230-038_ASPRAK KAK AKMAL

--- modul-5/230441100038_MIftahuzzainAbrori/MODUL5/MAIN.java
+++ modul-5/230441100038_MIftahuzzainAbrori/MODUL5/MAIN.java
@@ -44,3 +44,75 @@ public class MAIN {
         
     }
 }
+
+
+
+String camera = "Mengambil foto";
+    String film = "Merekam Vidio";
+    String kirim = "Mengirim File";
+    String terima = "Menerima File";
+    String perangkat = "Terhubung dengan perangkat lain ";
+    String Jaringan = "mencari jaringan wifi";
+    String wifi = "Terhhubung dengan wifi";
+    String lupakan = "Jaringan dilupakan";
+    String kordinat = "000000000000";        
+
+    public Smartphone(String merk, String model) {
+        super(merk, model);
+    }
+    
+    @Override
+    public void tampilkanInfo() {
+        System.out.println("=== Smartphone ===");
+        System.out.println("Merk: " + getMerk());
+        System.out.println("Model: " + getModel());
+    }
+
+    @Override
+    public void ambilFoto() {
+        System.out.println(" mengambil foto." + camera);
+    }
+
+    @Override
+    public void rekamVideo() {
+        System.out.println(" merekam video." + film);
+    }
+
+    @Override
+    public void kirimFile() {
+        System.out.println(" mengirim file." + kirim);
+    }
+
+    @Override
+    public void terimaFile() {
+        System.out.println(" menerima file." + terima);
+    }
+
+    @Override
+    public void koneksiPerangkat() {
+        System.out.println(" terhubung dengan perangkat lain." + perangkat);
+    }
+
+    @Override
+    public void cariJaringan() {
+        System.out.println(" mencari jaringan WiFi." + Jaringan);
+    }
+
+    @Override
+    public void hubunganJaringan() {
+        System.out.println(" terhubung ke jaringan WiFi." + wifi);
+    }
+
+    @Override
+    public void lupakanJaringan() {
+        System.out.println(" melupakan jaringan WiFi." + lupakan);
+    }
+
+    @Override
+    public void ambilKoordinat() {
+        System.out.println(" melupakan jaringan WiFi." + kordinat);
+    }
+
+}
+
+
