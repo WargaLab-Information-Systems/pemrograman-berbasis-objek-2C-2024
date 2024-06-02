81e6f396f06f8d509602456593151d5b95ffe1d6
RivalV124 <rivaljaya123@gmail.com>
2024-06-02 20:52:53 +0700

    Faroid_PBO2C_M6_140

---
+++ modul-6/230441100140_RivalValentinoHidayat/src/main/java/com/mycompany/gadget/Bluetooth.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gadget;
+
+/**
+ *
+ * @author LENOVO
+ */
+public interface Bluetooth {
+    void kirimFile();
+    void terimaFile();
+    void koneksiPerangkat(); 
+}
